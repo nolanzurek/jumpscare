27abeae7978d9ca9178564fd824e950cd3b397e0
Nolan Zurek <nolanzurek@gmail.com>
2024-08-05 18:17:25 -0600

    Added mathjax support (why not)

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,7 +1,14 @@
 import Markdown from "preact-markdown";
+import { useEffect } from "preact/hooks";
 import "./ListItem.css";
 
 export function ListItem(props: { text: string; removeItem: () => void }) {
+  // if the list changes, rerender the mathjax
+  useEffect(() => {
+    MathJax.typeset();
+    return () => MathJax.typesetClear();
+  }, [props.text]);
+
   return (
     <li className="list-item">
       <button className="close-button" onClick={props.removeItem}>
