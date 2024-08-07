49faab4f591865c14eabc920e775deae766f39e0
Nolan Zurek <nolanzurek@gmail.com>
2024-08-07 14:21:05 -0600

    bug fixes

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,17 +1,17 @@
 import Markdown from "preact-markdown";
-import { useEffect } from "preact/hooks";
+// import { useEffect } from "preact/hooks";
 import "./ListItem.css";
 
 export function ListItem(props: { text: string; removeItem: () => void }) {
-  // if the list changes, rerender the mathjax
-  useEffect(() => {
-    try {
-      MathJax.typeset();
-      return () => MathJax.typesetClear();
-    } catch (e) {
-      // womp womp
-    }
-  }, [props.text]);
+  // // if the list changes, rerender the mathjax
+  // useEffect(() => {
+  //   try {
+  //     MathJax.typeset();
+  //     return () => MathJax.typesetClear();
+  //   } catch (e) {
+  //     // womp womp
+  //   }
+  // }, [props.text]);
 
   return (
     <li className="list-item">
